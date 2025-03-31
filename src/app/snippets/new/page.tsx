f98f9247df10ceffbf2ab7a3bd75d9599647904e
Siddharth Vyas <siddharth.vyas@beyondkey.com>
2025-03-31 18:25:59 +0530

    changes

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,56 +1,58 @@
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
 
-import {db} from '@/db';
-import {redirect} from 'next/navigation';
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action!
+    'use server';
 
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
 
-    async function createSnippet(formData: FormData) {
-        'use server';
-        const title = formData.get('title')?.toString() || '';
-        const code = formData.get('code')?.toString() || '';
-        
-        const snippets = await db.snippet.create({
-            data: {
-                title,
-                code,
-            },
-        });
-        console.log(snippets);
-        redirect('/');
-    }
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
 
+    // Redirect the user back to the root route
+    redirect('/');
+  }
 
-    return (
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
 
-        <form action={createSnippet} >
-            <h3 className="font-bold m-3">Create a Snippet</h3>
-            <div className="flex flex-col gap-4">
-                <div className="flex gap-4">
-                    <label className="w-12" htmlFor="title">
-                        Title
-                    </label>
-                    <input
-                        name="title"
-                        className="border rounded p-2 w-full"
-                        id="title"
-                    />
-                </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
 
-                <div className="flex gap-4">
-                    <label className="w-12" htmlFor="code">
-                        Code
-                    </label>
-                    <textarea
-                        name="code"
-                        className="border rounded p-2 w-full"
-                        id="code"
-                    />
-                </div>
-
-                <button type="submit" className="rounded p-2 bg-blue-200">
-                    Create
-                </button>
-            </div>
-        </form>
-    );
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
